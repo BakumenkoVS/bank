6e307b67b0d4dd2a7a27b93773301b84b2ee7a2b
vlad bakumenko <tinyyy11@yandex.ru>
2023-06-14 09:12:59 +0300

    add core

---
+++ src/components/layout/layout.component.js
@@ -0,0 +1,20 @@
+export class Layout {
+	constructor({ router, children }) {
+		this.router = router
+		this.children = children
+	}
+	render() {
+		const headerHTML = "<header>Header</header>"
+		return `<header>
+        <nav>
+            <a href='/'>Home</a>
+            <a href='auth'>Auth</a>
+        </nav>
+        </header>
+        ${headerHTML}
+        <main>
+          ${this.children}
+        </main>
+        `
+	}
+}
